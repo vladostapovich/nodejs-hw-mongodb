099c68cb0a1764e6ffc0fe80c91047a7a6e86613
vladostapovich <jorabumer@ukr.net>
2025-07-23 16:05:46 +0300

    patchfix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -15,14 +15,9 @@ export const createContactsSchema = Joi.object({
 });
 
 export const updateContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string()
-    .pattern(/^\+?[0-9]{10,15}$/)
-    .required(),
-  email: Joi.string().email().required(),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().pattern(/^\+?[0-9]{10,15}$/),
+  email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-
-    .valid('work', 'home', 'personal')
-    .required(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
